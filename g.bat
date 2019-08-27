4e635e33c04d029b3800fbef49a06a1a068ab032
fananchong <fananchong@126.com>
2019-08-27 09:58:04 +0800

    init

---
+++ g.bat
@@ -0,0 +1,11 @@
+set GOPROXY=https://goproxy.io
+go get -u github.com/fananchong/test_protobuf_options
+go get github.com/golang/protobuf
+go list -m -f "{{.Dir}}" github.com/fananchong/test_protobuf_options > temp
+set /P DEP1=<temp
+go list -m -f "{{.Dir}}" github.com/golang/protobuf > temp
+set /P DEP2=<temp
+echo %DEP1%
+echo %DEP2%
+
+protoc -I. -I%DEP1% -I%DEP2% --go_out=plugins=grpc:. test.proto
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/fananchong/test_protobuf_options_example
+
+go 1.12
+
+require (
+	github.com/fananchong/test_protobuf_options v0.0.0-20190827014011-491ba0a38968
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.23.0
+)
